03a548bd04f215790cc85205e08364e880604db0
patte <p@tte.io>
2017-03-12 19:05:20 +0100

    terraform: reflect reality in kubernetes.tf

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -23,7 +23,7 @@ resource "exoscale_compute" "k8s-worker" {
   name = "k8s-worker-${count.index}"
   template = "Linux Ubuntu 16.04 LTS 64-bit"
   zone = "ch-dk-2"
-  size = "Tiny"
+  size = "Small"
   diskSize = 10
   keypair = "patrick.recher@project-r.construction"
   userdata = ""
@@ -36,7 +36,7 @@ resource "exoscale_compute" "k8s-worker-disk" {
   name = "k8s-worker-disk-${count.index}"
   template = "Linux Ubuntu 16.04 LTS 64-bit"
   zone = "ch-dk-2"
-  size = "Tiny"
+  size = "Small"
   diskSize = 50
   keypair = "patrick.recher@project-r.construction"
   userdata = ""
